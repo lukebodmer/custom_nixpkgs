ca88a95429bbe0f4943f585f8f7facf4d2e5b519
lukebodmer <lukejbodmer@gmail.com>
2025-11-03 18:03:54 -0500

    dev env fix
    fixed dev env

--- pkgs/dev-env/default.nix
+++ pkgs/dev-env/default.nix
@@ -1,12 +1,22 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
   flakeNixContent = builtins.readFile ./dev_flake.nix;
   envrcContent = builtins.readFile ./dev_envrc;
 in
 pkgs.writeShellScriptBin "dev-env" ''
-  printf '%s\n' '${flakeNixContent}' > flake.nix
-  printf '%s\n' '${envrcContent}' > .envrc
-  printf '%s\n' '.envrc\n.direnv/' > .gitignore
+  cat <<EOF > flake.nix
+${flakeNixContent}
+EOF
+
+  cat <<EOF > .envrc
+${envrcContent}
+EOF
+
+  cat <<EOF > .gitignore
+.envrc
+.direnv/
+EOF
+
   direnv allow
 ''
