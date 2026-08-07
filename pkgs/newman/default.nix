5dc88076ceb42f76b547a7229685bd6482e7fdb8
lukebodmer <lukejbodmer@gmail.com>
2026-08-07 12:29:22 -0700

    added emacs integration to newman

--- pkgs/newman/default.nix
+++ pkgs/newman/default.nix
@@ -8,12 +8,12 @@ python313.pkgs.buildPythonApplication {
   src = fetchFromGitHub {
     owner = "lukebodmer";
     repo = "newman";
-    rev = "6d85cf63e0f11159060672c1bf308d78cf1ef7ee";
-    sha256 = "sha256-CM5JpcCPaCx3zexTvVFCb9X7N65UXxZ8O1CqKHeSOI0=";
+    rev = "9a11d7c85fac22ac6b0b78d1101e5a81e520b538";
+    sha256 = "sha256-o3/KS04T66fF1DEMFCqHNV0hAsnpt6D3MM+Nnl1Tlyk=";
   };
 
   build-system = [ python313.pkgs.setuptools ];
-  dependencies = [ python313.pkgs.rich python313.pkgs.httpx ];
+  dependencies = [ python313.pkgs.rich python313.pkgs.httpx python313.pkgs.trafilatura ];
 
   meta = with lib; {
     description = "A personal AI agent harness for the terminal";
