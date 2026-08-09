9116c831e5bcf65264aa7549ed424024b4f67ba9
lukebodmer <lukejbodmer@gmail.com>
2026-08-09 14:37:39 -0700

    updated newman

--- pkgs/newman/default.nix
+++ pkgs/newman/default.nix
@@ -8,8 +8,8 @@ python313.pkgs.buildPythonApplication {
   src = fetchFromGitHub {
     owner = "lukebodmer";
     repo = "newman";
-    rev = "d1c33f282168b8a27bf9382d376b28d6c6e05695";
-    sha256 = "sha256-MByAUCxgrnvNeEGDiwJE6daa4uOO7pedsqFoyW0nH0c=";
+    rev = "967001a11be607962b5496b9a2612c58c3fe565b";
+    sha256 = "sha256-gd9WksHnudbRiEEIUb1D1UE85N8WOc/O9wv9wpsHYQA=";
   };
 
   build-system = [ python313.pkgs.setuptools ];
