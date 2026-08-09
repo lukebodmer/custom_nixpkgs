f46f4e31a7e0f4fb6b6f60a21a2bdb7b159d2541
lukebodmer <lukejbodmer@gmail.com>
2026-08-09 10:33:51 -0700

    updates to newman

--- pkgs/newman/default.nix
+++ pkgs/newman/default.nix
@@ -8,12 +8,12 @@ python313.pkgs.buildPythonApplication {
   src = fetchFromGitHub {
     owner = "lukebodmer";
     repo = "newman";
-    rev = "9a11d7c85fac22ac6b0b78d1101e5a81e520b538";
-    sha256 = "sha256-o3/KS04T66fF1DEMFCqHNV0hAsnpt6D3MM+Nnl1Tlyk=";
+    rev = "5f51f36759e4d589c1b58bc9c0b6be1ae33d0347";
+    sha256 = "sha256-rT4wKXn9SSOhukxNx6kZcGpI7u1XsrmIlzalGKGb42I=";
   };
 
   build-system = [ python313.pkgs.setuptools ];
-  dependencies = [ python313.pkgs.rich python313.pkgs.httpx python313.pkgs.trafilatura python313.pkgs.pypdf ];
+  dependencies = [ python313.pkgs.rich python313.pkgs.httpx python313.pkgs.trafilatura python313.pkgs.pypdf python313.pkgs.prompt-toolkit python313.pkgs.websockets ];
 
   meta = with lib; {
     description = "A personal AI agent harness for the terminal";
